368d33084d3b1491c5132cdcfd98a189a223064e
Harendra Bisht 2 <harendra.bisht@publicissapient.com>
2023-02-22 18:58:00 +0530

    updated docker image and service as load balancer

--- products-service/envs.ts
+++ products-service/envs.ts
@@ -4,6 +4,6 @@ config();
 
 export default {
     port: process.env.PORT || 4000,
-    graphqlPath: process.env.GRAPHQL_PATH || '/graphql',
+    graphqlPath: `/${process.env.GRAPHQL_PATH}` || '/graphql',
     dbUrl: process.env.DB_URL || 'mongodb://localhost:27017/graphql-starter',
 }
